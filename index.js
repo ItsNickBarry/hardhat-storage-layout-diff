746fcc5ef282af8bfd4d3775438085a6c6ef8582
Nick Barry <itsnickbarry@protonmail.ch>
2021-09-10 02:22:56 -0500

    add export task, add check task, rename diff task to compare task

--- index.js
+++ index.js
@@ -7,6 +7,108 @@ const {
   TASK_COMPILE,
 } = require('hardhat/builtin-tasks/task-names');
 
+const loadStorageLayout = async function (contractName, ref) {
+  const repository = simpleGit();
+  await repository.init();
+  const { latest } = await repository.log();
+
+  // TODO: error if ref === 'HEAD'
+
+  await repository.checkout(ref || latest.hash);
+  await hre.run(TASK_COMPILE);
+
+  const info = await hre.artifacts.getBuildInfo(contractName);
+  const [file, contract] = contractName.split(':');
+
+  await repository.checkout('-');
+
+  return parseStorageLayout(info.output.contracts[file][contract].storageLayout)
+}
+
+const parseStorageLayout = function ({ storage, types }) {
+  return storage.reduce(function (acc, { label, offset, slot, type}) {
+    const size = parseInt(types[type].numberOfBytes);
+    const bytesStart = parseInt(slot) * 32 + parseInt(offset);
+    const bytesEnd = bytesStart + size - 1;
+
+    if (acc.length > 0 && bytesStart > acc[acc.length - 1].bytesEnd + 1) {
+      acc.push({
+        label: '',
+        type: '',
+        bytesStart: acc[acc.length - 1].bytesEnd + 1,
+        bytesEnd: bytesStart - 1,
+      });
+    }
+
+    acc.push({
+      label,
+      type: types[type].label,
+      slot: parseInt(slot),
+      offset,
+      size,
+      bytesStart,
+      bytesEnd,
+    });
+
+    return acc;
+  }, []);
+};
+
+const mergeStorageLayouts = function (storageA, storageB) {
+  const equal = function (a, b) {
+    return a.label == b.label && a.type?.replace(/\[\d*\]/, '[]') == b.type?.replace(/\[\d*\]/, '[]');
+  };
+
+  let bytesIndex = 0;
+  let indexA = 0;
+  let indexB = 0;
+
+  // ensure even byte lengths
+
+  const tail = {
+    bytesEnd: Math.max(
+      storageA[storageA.length - 1].bytesEnd,
+      storageB[storageB.length - 1].bytesEnd
+    )
+  };
+
+  storageA.push(tail);
+  storageB.push(tail);
+
+  const output = [];
+
+  while (storageA[indexA] && storageB[indexB]) {
+    const elementA = storageA[indexA];
+    const elementB = storageB[indexB];
+
+    const bytesEndA = elementA.bytesEnd;
+    const bytesEndB = elementB.bytesEnd;
+
+    const bytesStart = bytesIndex;
+    const bytesEnd = Math.min(bytesEndA, bytesEndB);
+
+    output.push({
+      bytesStart,
+      bytesEnd,
+      elementA,
+      elementB,
+      changed: !equal(elementA, elementB),
+    });
+
+    if (bytesEndA >= bytesEndB) {
+      indexB++;
+    }
+
+    if (bytesEndB >= bytesEndA) {
+      indexA++;
+    }
+
+    bytesIndex = bytesEnd + 1;
+  }
+
+  return output;
+};
+
 task(TASK_COMPILE, async function (args, hre, runSuper) {
   for (let compiler of hre.config.solidity.compilers) {
     compiler.settings.outputSelection['*']['*'].push('storageLayout');
@@ -16,7 +118,77 @@ task(TASK_COMPILE, async function (args, hre, runSuper) {
 });
 
 task(
-  'storage-layout-diff'
+  'export-storage-layout'
+).setAction(async function () {
+  // TODO: define as Hardhat config
+  const config = { path: './storage_layout', only: [], except: [], spacing: 2 };
+
+  const outputDirectory = path.resolve(hre.config.paths.root, config.path);
+
+  if (!outputDirectory.startsWith(hre.config.paths.root)) {
+    throw new HardhatPluginError('resolved path must be inside of project directory');
+  }
+
+  if (outputDirectory === hre.config.paths.root) {
+    throw new HardhatPluginError('resolved path must not be root directory');
+  }
+
+  if (config.clear && fs.existsSync(outputDirectory)) {
+    fs.rmdirSync(outputDirectory, { recursive: true });
+  }
+
+  if (!fs.existsSync(outputDirectory)) {
+    fs.mkdirSync(outputDirectory, { recursive: true });
+  }
+
+  for (let contractName of await hre.artifacts.getAllFullyQualifiedNames()) {
+    if (config.only.length && !config.only.some(m => contractName.match(m))) continue;
+    if (config.except.length && config.except.some(m => contractName.match(m))) continue;
+
+    const info = await hre.artifacts.getBuildInfo(contractName);
+    const [file, contract] = contractName.split(':');
+    const { storage, types } = info.output.contracts[file][contract].storageLayout;
+
+    if (!storage.length) continue;
+
+    const destination = path.resolve(
+      outputDirectory,
+      config.flat ? '' : '',
+      contractName
+    ) + '.json';
+
+    if (!fs.existsSync(path.dirname(destination))) {
+      fs.mkdirSync(path.dirname(destination), { recursive: true });
+    }
+
+    fs.writeFileSync(destination, `${ JSON.stringify({ storage, types }, null, config.spacing) }\n`, { flag: 'w' });
+  }
+})
+
+task(
+  'storage-layout-check'
+).addParam(
+  'source', 'Path to storage layout JSON'
+).addParam(
+  'b', 'Contract to check against storage layout'
+).addOptionalParam(
+  'bRef', 'Git reference where contract B is defined'
+).setAction(async function ({ source, b, bRef }) {
+  const layout = parseStorageLayout(JSON.parse(fs.readFileSync(source)));
+  const storageB = await loadStorageLayout(b, bRef);
+  const data = mergeStorageLayouts(layout, storageB);
+
+  ejs.renderFile(path.resolve(__dirname, 'template.html.ejs'), { data, titleA: source, titleB: b }, {}, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      fs.writeFileSync('./out.html', result);
+    }
+  });
+});
+
+task(
+  'storage-layout-compare'
 ).addParam(
   'a', 'First contract to diff'
 ).addParam(
@@ -26,108 +198,8 @@ task(
 ).addOptionalParam(
   'bRef', 'Git reference where contract B is defined'
 ).setAction(async function ({ a, b, aRef, bRef }, hre) {
-  const repository = simpleGit();
-  await repository.init();
-
-  const { latest } = await repository.log();
-  aRef = aRef || latest.hash;
-  bRef = bRef || latest.hash;
-
-  const parseStorageLayout = async function (contractName, ref) {
-    await repository.checkout(ref);
-    await hre.run(TASK_COMPILE);
-
-    const info = await hre.artifacts.getBuildInfo(contractName);
-    const [file, contract] = contractName.split(':');
-    const { storage, types } = info.output.contracts[file][contract].storageLayout;
-
-    await repository.checkout('-');
-
-    return storage.reduce(function (acc, { label, offset, slot, type}) {
-      const size = parseInt(types[type].numberOfBytes);
-      const bytesStart = parseInt(slot) * 32 + parseInt(offset);
-      const bytesEnd = bytesStart + size - 1;
-
-      if (acc.length > 0 && bytesStart > acc[acc.length - 1].bytesEnd + 1) {
-        acc.push({
-          label: '',
-          type: '',
-          bytesStart: acc[acc.length - 1].bytesEnd + 1,
-          bytesEnd: bytesStart - 1,
-        });
-      }
-
-      acc.push({
-        label,
-        type: types[type].label,
-        slot: parseInt(slot),
-        offset,
-        size,
-        bytesStart,
-        bytesEnd,
-      });
-
-      return acc;
-    }, []);
-  };
-
-  const mergeStorageLayouts = function (storageA, storageB) {
-    const equal = function (a, b) {
-      return a.label == b.label && a.type?.replace(/\[\d*\]/, '[]') == b.type?.replace(/\[\d*\]/, '[]');
-    };
-
-    let bytesIndex = 0;
-    let indexA = 0;
-    let indexB = 0;
-
-    // ensure even byte lengths
-
-    const tail = {
-      bytesEnd: Math.max(
-        storageA[storageA.length - 1].bytesEnd,
-        storageB[storageB.length - 1].bytesEnd
-      )
-    };
-
-    storageA.push(tail);
-    storageB.push(tail);
-
-    const output = [];
-
-    while (storageA[indexA] && storageB[indexB]) {
-      const elementA = storageA[indexA];
-      const elementB = storageB[indexB];
-
-      const bytesEndA = elementA.bytesEnd;
-      const bytesEndB = elementB.bytesEnd;
-
-      const bytesStart = bytesIndex;
-      const bytesEnd = Math.min(bytesEndA, bytesEndB);
-
-      output.push({
-        bytesStart,
-        bytesEnd,
-        elementA,
-        elementB,
-        changed: !equal(elementA, elementB),
-      });
-
-      if (bytesEndA >= bytesEndB) {
-        indexB++;
-      }
-
-      if (bytesEndB >= bytesEndA) {
-        indexA++;
-      }
-
-      bytesIndex = bytesEnd + 1;
-    }
-
-    return output;
-  };
-
-  const storageA = await parseStorageLayout(a, aRef);
-  const storageB = await parseStorageLayout(b, bRef);
+  const storageA = await loadStorageLayout(a, aRef);
+  const storageB = await loadStorageLayout(b, bRef);
   const data = mergeStorageLayouts(storageA, storageB);
 
   ejs.renderFile(path.resolve(__dirname, 'template.html.ejs'), { data, titleA: a, titleB: b }, {}, function (err, result) {
