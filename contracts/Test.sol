c8f22b8f00500e7789d2f81112ed1984c15b947a
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-18 16:36:46 -0600

    update manual test contract

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -2,14 +2,22 @@
 pragma solidity *;
 
 contract Test {
-    bytes32 a;
-}
-contract Test2 {
-    bytes32 a;
-    bytes32 b;
-    bytes16 c;
-    bool d;
-    bytes4 dd;
-    address e;
-    uint256 f;
+    enum E {
+        ONE,
+        TWO
+    }
+
+    struct Struct {
+        address a;
+        E e;
+        address b;
+        E f;
+    }
+
+    Struct str;
+    mapping(address => bool) map;
+    uint128[5] five;
+    bool b0;
+    bool b1;
+    bool[4][2] bools;
 }
