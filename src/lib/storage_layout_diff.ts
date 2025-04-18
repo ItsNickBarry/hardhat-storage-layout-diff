de1e39fa5cc673574d1f95b27496aa82b29bdf2f
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-18 13:36:34 -0600

    move inspect-storage-layout slot processing to helper funtion

--- src/lib/storage_layout_diff.ts
+++ src/lib/storage_layout_diff.ts
@@ -21,11 +21,13 @@ export type StorageElement = {
   type: string;
 };
 
+export type StorageLayoutTypes = {
+  [name: string]: StorageType;
+};
+
 export type StorageLayout = {
   storage: StorageElement[];
-  types: {
-    [name: string]: StorageType;
-  };
+  types: StorageLayoutTypes;
 };
 
 export type ParsedStorageElement = Partial<
@@ -33,6 +35,19 @@ export type ParsedStorageElement = Partial<
 > & {
   bytesStart: number;
   bytesEnd: number;
+};
+
+type Entry = {
+  type: string;
+  label: string;
+  sizeFilled?: number;
+};
+
+type Slot = {
+  id: bigint;
+  sizeReserved: number;
+  sizeFilled: number;
+  entries: Entry[];
 };
 
 export const visualizeSlot = (
@@ -193,4 +208,115 @@ export const mergeStorageLayouts = function (
   }
 
   return output;
+};
+
+export const collateSlotEntries = (
+  types: StorageLayoutTypes,
+  storage: Entry[],
+) => {
+  const slots: Slot[] = [
+    { id: 0n, sizeReserved: 0, sizeFilled: 0, entries: [] },
+  ];
+
+  const proc = (slots: Slot[], entries: Entry[], index = 0) => {
+    let slot = slots[slots.length - 1];
+
+    const entry = entries[index];
+    if (!entry) return;
+    const type = types[entry.type];
+
+    // create a new slot if current entry requires it
+
+    if (
+      slot.entries.length !== 0 &&
+      32 - slot.sizeReserved < Number(type.numberOfBytes)
+    ) {
+      slot = {
+        id: slot.id + 1n,
+        sizeReserved: 0,
+        sizeFilled: 0,
+        entries: [],
+      };
+      slots.push(slot);
+    }
+
+    if (type.encoding === 'mapping') {
+      // a mapping slot reserves 32 bytes, but writes no data
+      const sizeReserved = 32;
+      const sizeFilled = 0;
+
+      slot.sizeReserved += sizeReserved;
+      slot.sizeFilled += sizeFilled;
+
+      slot.entries.push({
+        type: entry.type,
+        label: entry.label,
+        sizeFilled,
+      });
+    } else if (type.encoding === 'dynamic_array') {
+      // a dynamic array slot stores the array length using 32 bytes
+      const sizeReserved = 32;
+      const sizeFilled = 32;
+
+      slot.sizeReserved += sizeReserved;
+      slot.sizeFilled += sizeFilled;
+
+      slot.entries.push({
+        type: entry.type,
+        label: entry.label,
+        sizeFilled,
+      });
+    } else {
+      // type encoding is 'inplace', and might not fill its slot
+      if (type.members) {
+        // type is a struct
+
+        const members: Entry[] = type.members.map((m) => ({
+          type: m.type,
+          label: `${entry.label}.${m.label}`,
+        }));
+
+        proc(slots, members);
+
+        // struct reserves the entirety of its final slot
+        // retrieve the slot from the array in case a new one was added during the recursive call
+        slots[slots.length - 1].sizeReserved = 32;
+      } else if (type.base) {
+        // type is a fixed array
+
+        const [, count] = type.label.match(/.+\[(\d+)\]$/)!;
+
+        const members: Entry[] = [];
+
+        for (let i = 0; i < Number(count); i++) {
+          members.push({ type: type.base, label: `${entry.label}[${i}]` });
+        }
+
+        proc(slots, members);
+
+        // array reserves the entirety of its final slot
+        // retrieve the slot from the array in case a new one was added during the recursive call
+        slots[slots.length - 1].sizeReserved = 32;
+      } else {
+        // type is a value type
+        const sizeReserved = Number(type.numberOfBytes);
+        const sizeFilled = sizeReserved;
+
+        slot.sizeReserved += sizeReserved;
+        slot.sizeFilled += sizeFilled;
+
+        slot.entries.push({
+          type: entry.type,
+          label: entry.label,
+          sizeFilled,
+        });
+      }
+    }
+
+    proc(slots, entries, index + 1);
+  };
+
+  proc(slots, storage);
+
+  return slots;
 };
--- src/tasks/inspect_storage_layout.ts
+++ src/tasks/inspect_storage_layout.ts
@@ -1,4 +1,8 @@
-import { getStorageLayout, visualizeSlot } from '../lib/storage_layout_diff';
+import {
+  collateSlotEntries,
+  getStorageLayout,
+  visualizeSlot,
+} from '../lib/storage_layout_diff';
 import { TASK_INSPECT_STORAGE_LAYOUT } from '../task_names';
 import Table from 'cli-table3';
 import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
@@ -39,122 +43,7 @@ task(TASK_INSPECT_STORAGE_LAYOUT)
       { content: 'visualization (right to left)' },
     ]);
 
-    type Entry = {
-      type: string;
-      label: string;
-      sizeFilled?: number;
-    };
-
-    type Slot = {
-      id: bigint;
-      sizeReserved: number;
-      sizeFilled: number;
-      entries: Entry[];
-    };
-
-    const slots: Slot[] = [
-      { id: 0n, sizeReserved: 0, sizeFilled: 0, entries: [] },
-    ];
-
-    const proc = (slots: Slot[], entries: Entry[], index = 0) => {
-      let slot = slots[slots.length - 1];
-
-      const entry = entries[index];
-      if (!entry) return;
-      const type = types[entry.type];
-
-      // create a new slot if current entry requires it
-
-      if (
-        slot.entries.length !== 0 &&
-        32 - slot.sizeReserved < Number(type.numberOfBytes)
-      ) {
-        slot = {
-          id: slot.id + 1n,
-          sizeReserved: 0,
-          sizeFilled: 0,
-          entries: [],
-        };
-        slots.push(slot);
-      }
-
-      if (type.encoding === 'mapping') {
-        // a mapping slot reserves 32 bytes, but writes no data
-        const sizeReserved = 32;
-        const sizeFilled = 0;
-
-        slot.sizeReserved += sizeReserved;
-        slot.sizeFilled += sizeFilled;
-
-        slot.entries.push({
-          type: entry.type,
-          label: entry.label,
-          sizeFilled,
-        });
-      } else if (type.encoding === 'dynamic_array') {
-        // a dynamic array slot stores the array length using 32 bytes
-        const sizeReserved = 32;
-        const sizeFilled = 32;
-
-        slot.sizeReserved += sizeReserved;
-        slot.sizeFilled += sizeFilled;
-
-        slot.entries.push({
-          type: entry.type,
-          label: entry.label,
-          sizeFilled,
-        });
-      } else {
-        // type encoding is 'inplace', and might not fill its slot
-        if (type.members) {
-          // type is a struct
-
-          const members: Entry[] = type.members.map((m) => ({
-            type: m.type,
-            label: `${entry.label}.${m.label}`,
-          }));
-
-          proc(slots, members);
-
-          // struct reserves the entirety of its final slot
-          // retrieve the slot from the array in case a new one was added during the recursive call
-          slots[slots.length - 1].sizeReserved = 32;
-        } else if (type.base) {
-          // type is a fixed array
-
-          const [, count] = type.label.match(/.+\[(\d+)\]$/)!;
-
-          const members: Entry[] = [];
-
-          for (let i = 0; i < Number(count); i++) {
-            members.push({ type: type.base, label: `${entry.label}[${i}]` });
-          }
-
-          proc(slots, members);
-
-          // array reserves the entirety of its final slot
-          // retrieve the slot from the array in case a new one was added during the recursive call
-          slots[slots.length - 1].sizeReserved = 32;
-        } else {
-          // type is a value type
-          const sizeReserved = Number(type.numberOfBytes);
-          const sizeFilled = sizeReserved;
-
-          slot.sizeReserved += sizeReserved;
-          slot.sizeFilled += sizeFilled;
-
-          slot.entries.push({
-            type: entry.type,
-            label: entry.label,
-            sizeFilled,
-          });
-        }
-      }
-
-      proc(slots, entries, index + 1);
-    };
-
-    proc(slots, storage);
+    const slots = collateSlotEntries(types, storage);
 
     for (const slot of slots) {
       let offset = 0;
