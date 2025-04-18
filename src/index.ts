95acc5cd0d5d0edc73355a018f35e910b0019b1b
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-17 22:58:45 -0600

    add draft inspect task

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import pkg from '../package.json';
 import {
   TASK_EXPORT_STORAGE_LAYOUT,
+  TASK_INSPECT_STORAGE_LAYOUT,
   TASK_STORAGE_LAYOUT_CHECK,
   TASK_STORAGE_LAYOUT_COMPARE,
 } from './task_names';
 import './type_extensions';
 import type { StorageLayoutDiffConfig } from './types';
+import Table from 'cli-table3';
 import ejs from 'ejs';
 import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
 import { extendConfig } from 'hardhat/config';
@@ -287,4 +289,60 @@ task(TASK_STORAGE_LAYOUT_COMPARE)
     );
 
     await fs.promises.writeFile('./out.html', contents);
+  });
+
+task(TASK_INSPECT_STORAGE_LAYOUT)
+  .addPositionalParam('contract', 'Contract whose storage layout to inspect')
+  .setAction(async (args, hre) => {
+    const { storage, types } = await getStorageLayout(hre, args.contract);
+
+    const table = new Table({
+      style: { head: [], border: [], 'padding-left': 2, 'padding-right': 2 },
+      chars: {
+        mid: '·',
+        'top-mid': '|',
+        'left-mid': ' ·',
+        'mid-mid': '|',
+        'right-mid': '·',
+        left: ' |',
+        'top-left': ' ·',
+        'top-right': '·',
+        'bottom-left': ' ·',
+        'bottom-right': '·',
+        middle: '·',
+        top: '-',
+        bottom: '-',
+        'bottom-mid': '|',
+      },
+    });
+
+    table.push([
+      { content: 'slot' },
+      { content: 'offset' },
+      { content: 'type' },
+      { content: 'name' },
+      { content: 'visualization (right to left)' },
+    ]);
+
+    const filled = '▰';
+    const empty = '▱';
+
+    for (const entry of storage) {
+      const size = parseInt(types[entry.type].numberOfBytes);
+
+      table.push([
+        { content: entry.slot },
+        { content: entry.offset },
+        { content: entry.type },
+        { content: entry.label },
+        {
+          content:
+            empty.repeat(32 - size - entry.offset) +
+            filled.repeat(size) +
+            empty.repeat(entry.offset),
+        },
+      ]);
+    }
+
+    console.log(table.toString());
   });
