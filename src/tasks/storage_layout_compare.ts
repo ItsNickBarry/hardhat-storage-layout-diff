a82b58d26c891a7d573260dfd2c3b489b925209a
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-19 03:45:37 -0600

    remove ejs and html output, replace old parse and merge functions

--- src/tasks/storage_layout_compare.ts
+++ src/tasks/storage_layout_compare.ts
@@ -1,12 +1,9 @@
 import {
   loadStorageLayout,
-  mergeStorageLayouts,
+  mergeCollatedSlots,
 } from '../lib/storage_layout_diff';
 import { TASK_STORAGE_LAYOUT_COMPARE } from '../task_names';
-import ejs from 'ejs';
 import { task } from 'hardhat/config';
-import fs from 'node:fs';
-import path from 'node:path';
 
 task(TASK_STORAGE_LAYOUT_COMPARE)
   .addParam('a', 'First contract to diff')
@@ -14,14 +11,9 @@ task(TASK_STORAGE_LAYOUT_COMPARE)
   .addOptionalParam('aRef', 'Git reference where contract A is defined')
   .addOptionalParam('bRef', 'Git reference where contract B is defined')
   .setAction(async function ({ a, b, aRef, bRef }, hre) {
-    const storageA = await loadStorageLayout(hre, a, aRef);
-    const storageB = await loadStorageLayout(hre, b, bRef);
-    const data = mergeStorageLayouts(storageA, storageB);
+    const slotsA = await loadStorageLayout(hre, a, aRef);
+    const slotsB = await loadStorageLayout(hre, b, bRef);
+    const data = mergeCollatedSlots(slotsA, slotsB);
 
-    const contents = await ejs.renderFile(
-      path.resolve(__dirname, 'template.html.ejs'),
-      { data, titleA: a, titleB: b },
-    );
-
-    await fs.promises.writeFile('./out.html', contents);
+    console.log(data);
   });
